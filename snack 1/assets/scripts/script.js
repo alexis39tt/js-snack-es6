7a9ec0e9fc10d8aca59cc0b4c42df23b82f78b98
alexis39tt <alessandro.fonzi@gmail.com>
2022-03-25 14:28:12 +0100

    snack 1 completed

---
+++ snack 1/assets/scripts/script.js
@@ -0,0 +1,45 @@
+/* Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+*/
+
+let bikes = [
+    {
+        'name': 'Superspeed XR3',
+        'weight': '27.3'
+    },
+    {
+        'name': 'Bianchi Specialissima T4',
+        'weight': '28.8'
+    },
+    {
+        'name': 'Carrera SL7',
+        'weight': '31.3'
+    },
+    {
+        'name': 'Pininfarina model 2',
+        'weight': '34.2'
+    },
+    {
+        'name': 'Belador BR',
+        'weight': '25.9'
+    },
+    {
+        'name': 'Corratec CCT Evo',
+        'weight': '29.6'
+    },
+    {
+        'name': 'Boardman SLR 9.6',
+        'weight': '30.5'
+    },
+];
+
+let container = document.querySelector(`#container`);
+let lightest = 0;
+
+for(i=0; i<bikes.length; i++){
+    let {weight} = bikes[i];
+    if(weight < weight[i-1]){
+        container.innerHTML += `<h1>La bici più leggera pesa ${weight}kg</h1>`;
+    }
+}
